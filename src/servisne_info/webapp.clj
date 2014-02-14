abe6d1d79e268da2f3249cde2dbaeca54214318b
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-02-14 12:13:12 +0100

    Run initialization

--- src/servisne_info/webapp.clj
+++ src/servisne_info/webapp.clj
@@ -5,4 +5,5 @@
 (defn -main [& args]
   (let [port (Integer/parseInt (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_PORT" "8080"))
         ip (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")]
-      (run-server handler/app {:ip ip :port port})))
+    (run-server handler/app {:ip ip :port port})
+    (handler/init)))
