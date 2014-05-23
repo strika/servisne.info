52287522af9bb7a8c4ce1ecf8043aa9080818727
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-05-23 15:27:04 +0200

    Log scrape

--- src/servisne_info/tasks/hello_world.clj
+++ src/servisne_info/tasks/hello_world.clj
@@ -1,4 +1,6 @@
-(ns servisne-info.tasks.hello-world)
+(ns servisne-info.tasks.hello-world
+  (:require [taoensso.timbre :as timbre]))
 
 (defn -main [& args]
+  (timbre/info "Hello World!")
   (println "Hello World!"))
--- src/servisne_info/tasks/scrape.clj
+++ src/servisne_info/tasks/scrape.clj
@@ -1,5 +1,8 @@
 (ns servisne-info.tasks.scrape
-  (:use servisne-info.scrape.worker))
+  (:use servisne-info.scrape.worker)
+  (:require [taoensso.timbre :as timbre]))
 
 (defn -main [& args]
-  (save-links))
+  (timbre/info "Scrape start")
+  (save-links)
+  (timbre/info "Scrape end"))
