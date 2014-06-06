36b16d494c6693f2c6b062ee42a9ff8470b66001
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-06-06 12:20:11 +0200

    Don't save a link if the link already exists

--- src/servisne_info/scrape/worker.clj
+++ src/servisne_info/scrape/worker.clj
@@ -3,8 +3,14 @@
   (:require [monger.collection :as mc]
             [servisne-info.scrape.ns-rs :as ns-scraper]))
 
+(defn- new-link? [link]
+  (nil? (mc/find-one "news" {:url (:url link)})))
+
+(defn- select-new-links [links]
+  (filter new-link? links))
+
 (defn save-links []
   (let [links (ns-scraper/links)]
-    (doseq [link links]
+    (doseq [link (select-new-links links)]
       (mc/insert "news" (assoc link :created-at (now))))
     (count links)))
--- test/servisne_info/test/scrape/worker_test.clj
+++ test/servisne_info/test/scrape/worker_test.clj
@@ -12,13 +12,18 @@
   (let [power-outage {:title "Power outage"
                       :url "http://example.com/power_outage"}]
     (with-redefs [ns-scraper/links (constantly [power-outage])]
+      (let [links-count (save-links)]
 
-      (testing "it returns links count"
-        (let [links-count (save-links)]
-          (is (= 1 links-count))))
+        (testing "it returns links count"
+          (is (= 1 links-count)))
 
-      (testing "it saves links to the database"
-        (let [news (mc/find-maps "news")
-              latest (first news)]
-        (is (= (:title latest) (:title power-outage)))
-        (is (= (:url latest) (:url power-outage))))))))
+        (testing "it saves links to the database"
+          (let [news (mc/find-maps "news")
+                latest (first news)]
+            (is (= (:title latest) (:title power-outage)))
+            (is (= (:url latest) (:url power-outage)))))
+
+        (testing "when the link was already saved to the database"
+          (save-links)
+          (let [news (mc/find-maps "news")]
+            (is (= (count news) 1))))))))
