21871e2ea180fa9299480c854f8234f22d437190
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-06-06 14:25:14 +0200

    Show info from db on the homepage

--- src/servisne_info/routes/home.clj
+++ src/servisne_info/routes/home.clj
@@ -1,13 +1,20 @@
 (ns servisne-info.routes.home
   (:use compojure.core)
-  (:require [servisne-info.views.layout :as layout]
+  (:require [monger.query :as mq]
+            [servisne-info.views.layout :as layout]
             [servisne-info.scrape.ns-rs :as scrape]))
 
+(defn- latest-news []
+  (mq/with-collection "news"
+    (mq/find {})
+    (mq/fields [:url :title :created-at])
+    (mq/sort (array-map :created-at 1))
+    (mq/limit 10)))
+
 (defn home-page []
-  (let [info-links (scrape/links)]
-    (layout/render
-      "home.html"
-      {:info-links info-links})))
+  (layout/render
+    "home.html"
+    {:info-links (latest-news)}))
 
 (defn about-page []
   (layout/render "about.html"))
--- test/servisne_info/features/homepage.clj
+++ test/servisne_info/features/homepage.clj
@@ -1,11 +1,23 @@
 (ns servisne-info.features.homepage
   (:use kerodon.core
         kerodon.test
-        clojure.test)
-  (:require [servisne-info.handler :refer [app]]))
+        clojure.test
+        servisne-info.utils
+        servisne-info.features.common)
+  (:require [monger.collection :as mc]
+            [servisne-info.handler :refer [app]]))
+
+(use-fixtures :once fixtures-once)
+(use-fixtures :each fixtures-each)
 
 (deftest homepage
-  (-> (session app)
-      (visit "/")
-      (within [:div.jumbotron :h2]
-        (has (text? "Dobrodošli na servisne.info")))))
+  (let [link {:title "Isključenja struje za 20. januar"
+              :url "http://www.021.rs/Novi-Sad/Servisne-informacije/Iskljucenja-struje-za-20-januar-2.html"
+              :created-at (now)}]
+    (mc/insert "news" link)
+    (-> (session app)
+        (visit "/")
+        (within [:div.jumbotron :h2]
+          (has (text? "Dobrodošli na servisne.info")))
+        (within [:div.latest-info :li]
+          (has (text? (:title link)))))))
