88fdcd3bb68a72015380ed695dcd697e43f13b56
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-06-20 12:06:13 +0200

    Fuzzy text search

--- src/servisne_info/utils.clj
+++ src/servisne_info/utils.clj
@@ -1,4 +1,15 @@
-(ns servisne-info.utils)
+(ns servisne-info.utils
+  (:use fuzzy-string.core))
 
 (defn now []
   (java.util.Date.))
+
+(defn str-partition [s n]
+  (map 
+    #(apply str (take n (drop % s)))
+    (range 0 (inc (- (count s) n)))))
+
+(defn fuzzy-has-text? [s text]
+  (let [partitioned-text (str-partition text (count s))]
+    (not-every? #(> % 5)
+                (map #(levenshtein s %) partitioned-text))))
