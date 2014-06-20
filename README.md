e8ec0441a0379eaadb21097edfb431ef38762cc9
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-06-20 11:02:37 +0200

    Send emails

--- README.md
+++ README.md
@@ -8,6 +8,16 @@ Visit http://servisne.info to try it out.
 [![Build
 Status](https://semaphoreapp.com/api/v1/projects/c1e6a8c7-fd01-4591-9553-1c26bf413ea2/174528/badge.png)](https://semaphoreapp.com)
 
+### Development
+
+Export following environment variables:
+
+```bash
+export EMAIL_HOST=smtp.mandrillapp.com
+export EMAIL_USER=user@example.com
+export EMAIL_PASS=myapikey
+```
+
 ### Deploy to OpenShift
 
 Create an account at Red Hat's [OpenShift](https://openshift.redhat.com). Install the CLI client.
---
+++ src/servisne_info/notifications.clj
@@ -0,0 +1,23 @@
+(ns servisne-info.notifications
+  (:use postal.core)
+  (:require [environ.core :refer [env]]
+            [servisne-info.views.email :as email]))
+
+(def default-from "nebojsa.stricevic+servisne@gmail.com")
+
+(defn send-email
+  [to subject body]
+  (send-message {:host (env :email-host)
+                 :user (env :email-user) 
+                 :pass (env :email-pass)
+                 :port 587}
+                {:from default-from
+                 :to to
+                 :subject subject
+                 :body body}))
+
+(defn send-test-email []
+  (send-email "nebojsa.stricevic@gmail.com"
+              "Test poruka"
+              (email/render "new_links.txt"
+                            {:links ["http://servisne.info/" "http://servisne.info/about"]})))
