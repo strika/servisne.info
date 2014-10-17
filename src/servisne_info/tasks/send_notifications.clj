5da80370c85082c70eef77783dfe1436abe01ef3
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-10-17 08:05:33 +0200

    Find news for multi-word street name

--- src/servisne_info/tasks/send_notifications.clj
+++ src/servisne_info/tasks/send_notifications.clj
@@ -1,24 +1,14 @@
 (ns servisne-info.tasks.send-notifications
-  (:use [clojure.string :only [join split]]
-        [servisne-info.notifications :only [send-news-email]]
+  (:use [servisne-info.notifications :only [send-news-email]]
         [servisne-info.tasks.task-definition :only [deftask]])
   (:require [servisne-info.logging :as l]
             [servisne-info.repository :as repo]))
 
 ; Private
-(defn escape-street [street]
-  (if (> (count (split street #" ")) 1)
-    (str "\\\"" street "\\\"")
-    street))
-
-(defn escape-streets [user]
-  (map escape-street (:streets user)))
-
-(defn find-news-for-user [user]
-  (let [streets (escape-streets user)
-        news (flatten (map #(repo/search-news %) streets))
+(defn find-news-for-user [{:keys [email streets] :as user}]
+  (let [news (flatten (map #(repo/search-news %) streets))
         unsent-news (filter #(nil? (:sent %)) news)]
-    (l/info "Finding news for user" {:user (:email user) :streets streets :news_count (count unsent-news)})
+    (l/info "Finding news for user" {:user email :streets streets :news_count (count unsent-news)})
     [user unsent-news]))
 
 (defn find-news-for-users [users]
