12292f32260bcc26af6f31768b911713573b2636
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-10-31 10:36:52 +0100

    Update org.clojure/tools.nrepl 0.2.3 -> 0.2.6

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [http-kit "2.1.19"]
                  [kerodon "0.4.0"]
                  [lib-noir "0.9.4"]
-                 [org.clojure/tools.nrepl "0.2.3"]
+                 [org.clojure/tools.nrepl "0.2.6"]
                  [overtone/at-at "1.2.0"]
                  [prone "0.6.0"]
                  [ring-server "0.3.1"]
