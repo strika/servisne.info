506242fdb197566a4e2f5b6822750e7ac2107387
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2014-11-07 11:52:07 +0100

    Handle subscription without email

--- src/servisne_info/controllers/users.clj
+++ src/servisne_info/controllers/users.clj
@@ -18,10 +18,14 @@
       (repo/delete-user email)))
   (layout/render "users/destroy.html"))
 
-(defn users-create [email streets]
+(defn create-or-update-user [email streets]
   (let [streets-seq (split-streets streets)]
     (if (repo/find-user email)
       (repo/update-user email {:streets streets-seq})
-      (repo/create-user {:email email :streets streets-seq})))
+      (repo/create-user {:email email :streets streets-seq}))))
+
+(defn users-create [email streets]
+  (if email
+    (create-or-update-user email streets))
   (layout/render "users/create.html"
                  {:email email :streets streets}))
