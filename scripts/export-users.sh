e6a109edb10146d266f6e3800f6ee0a7d777608e
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2015-01-04 12:33:29 +0100

    Export users to data directory

--- scripts/export-users.sh
+++ scripts/export-users.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-mongoexport --host $OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT \
+rm $OPENSHIFT_DATA_DIR/users.json
+
+mongoexport \
+  --host $OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT \
   --username $OPENSHIFT_MONGODB_DB_USERNAME \
   --password $OPENSHIFT_MONGODB_DB_PASSWORD \
   --db servisneinfo \
   --collection users \
-  --out users.json \
+  --out $OPENSHIFT_DATA_DIR/users.json \
   --journal
