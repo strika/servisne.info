cc8819fc0159fda8dc2a239e6310ffc52a1707d1
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2018-01-07 21:43:23 +0100

    Fix export users script

--- scripts/export-users.sh
+++ scripts/export-users.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-rm $OPENSHIFT_DATA_DIR/users.json
+rm ~/data/users.json
 
 mongoexport \
-  --host $OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT \
-  --username $OPENSHIFT_MONGODB_DB_USERNAME \
-  --password $OPENSHIFT_MONGODB_DB_PASSWORD \
   --db servisneinfo \
   --collection users \
-  --out $OPENSHIFT_DATA_DIR/users.json \
+  --out ~/data/users.json \
   --journal
